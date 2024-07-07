4b1f555ec6e99c88bc731591591a978ca79341ed
Raleydi Wardhana <166751493+wraleydi@users.noreply.github.com>
2024-07-07 22:01:17 +0700

    Update script.js

--- Calculator-App/script.js
+++ Calculator-App/script.js
@@ -4,28 +4,62 @@ const buttons = document.querySelectorAll('.button');
 let inputNow = '';
 let inputPrev = '';
 let operator = '';
+let result = 0;
+let isResultShown = false;
 
 buttons.forEach(button => {
-    button.addEventListener('click', function(){
-        if (button.hasAttribute('data-number')){
+    button.addEventListener('click', function() {
+        if (button.hasAttribute('data-number')) {
             handleNumber(button.innerText);
-        } else if (button.hasAttribute('data-operator')){
+        } else if (button.hasAttribute('data-operator')) {
             handleOperator(button.innerText);
-        } else if (button.hasAttribute('data-percent')){
-            handlePercent(button.innerText);
-        } else if (button.hasAttribute('data-del')){
+        } else if (button.hasAttribute('data-percent')) {
+            handlePercent();
+        } else if (button.hasAttribute('data-del')) {
             del();
-        } else if (button.hasAttribute('data-clear')){
+        } else if (button.hasAttribute('data-clear')) {
             clear();
-        } else if (button.hasAttribute('data-result')){
-            calculate();
+        } else if (button.hasAttribute('data-koma')) {
+            koma();
+        } else if (button.hasAttribute('data-result')) {
+            handleEqual();
         }
     });
 });
 
-function handleNumber(number){
-    if (inputNow.length < 15){
+function handleNumber(number) {
+    if (isResultShown) {
+        // Jika hasil sudah ditampilkan, mulai hitungan baru
+        clear();
+        isResultShown = false;
+    }
+    if (inputNow.length < 15) {
         inputNow += number;
         display.innerText = inputNow;
     }
 }
+
+function handlePercent() {
+    if (!inputNow) return;
+    inputNow = (parseFloat(inputNow) / 100).toString();
+    display.innerText = inputNow + '%';
+}
+
+function del() {
+    inputNow = inputNow.slice(0, -1);
+    if (inputNow === "") {
+        display.innerText = '0';
+    } else {
+        display.innerText = inputNow;
+    }
+}
+
+function clear() {
+    inputNow = '';
+    inputPrev = '';
+    operator = '';
+    result = 0;
+    display.innerText = '0';
+    isResultShown = false;
+}
+
